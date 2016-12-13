867a5eff97553dad963394b1402129dffe7a7784
Michael Jones <michael@autorefactorer.com>
2016-12-12 21:26:33 -0600

    completed check-rsvp script, and began work on send-rsvp script

--- check-rsvp/index.php
+++ check-rsvp/index.php
@@ -1,26 +1,45 @@
 <?php
-$credentials = parse_ini_file("/srv/db.ini");
-$sql_connection = new mysqli($credentials['host'],$credentials['username'],$credentials['password'],$credentials['db']);
-if ($sql_connection->connect_errno) {
-    printf("Connect failed: %s\n", $sql_connection->connect_error);
+if( $_POST['first_name'] && $_POST['last_name'] ) {
+    if (preg_match("/[^A-Za-z'-]/",$_POST['first_name']) || preg_match("/[^A-Za-z'-]/",$_POST['last_name'])) {
+        die ("error");
+    }
+    $first_name = strtolower($_POST['first_name']);
+    $last_name = strtolower($_POST['last_name']);
+
+    $credentials = parse_ini_file("/srv/db.ini");
+    $sql_connection = new mysqli($credentials['host'],$credentials['username'],$credentials['password'],$credentials['db']);
+    if ($sql_connection->connect_errno) {
+        die("error");
+    }
+
+    $query = "SELECT * FROM guest_list WHERE first_name = '$first_name' AND last_name = '$last_name'";
+    $results = $sql_connection->query($query) or die('error');
+    $row_count = $results->num_rows;
+    $results->close();
+
+    if($row_count > 0) {
+        echo "already_guest";
+    } else {
+        $query = "SELECT * FROM invited_list WHERE first_name = '$first_name' AND last_name = '$last_name'";
+        $results = $sql_connection->query($query) or die('error');
+        $row_count = $results->num_rows;
+        $results->close();
+
+	if($row_count == 0) {
+            echo "not_invited";
+        } else {
+            echo "can_rsvp";
+        }
+    }
     exit();
 }
-
-$query = "SELECT * FROM guest_list";
-$results = $sql_connection->query($query) or die('Error');
-$row_count = $results->num_rows;
-$results->close();
-if($row_count > 0) {
-	echo "already_guest";
-} else {
-	$query = "SELECT * FROM invited_list";
-	$results = $sql_connection->query($query) or die('Error');
-	$row_count = $results->num_rows;
-	$results->close();
-	if($row_count == 0) {
-		echo "not_invited";
-	} else {
-		echo "can_rsvp";
-	}
-}
 ?>
+<html>
+   <body>
+      <form action = "<?php $_PHP_SELF ?>" method = "POST">
+         First: <input type = "text" name = "first_name" />
+         Last: <input type = "text" name = "last_name" />
+         <input type = "submit" />
+      </form>
+   </body>
+</html>
---
+++ send-rsvp/index.php
@@ -0,0 +1,26 @@
+<?php
+if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0) {
+    $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+    if(strcasecmp($contentType, 'application/json') != 0) {
+         die('error');
+    }
+
+    $content = trim(file_get_contents("php://input"));           
+
+    $decoded = json_decode($content, true);
+
+    if(!is_array($decoded)) {
+        die('error');
+    }
+}
+?>
+
+<html>
+   <body>
+      <form action = "<?php $_PHP_SELF ?>" method = "POST">
+         First: <input type = "text" name = "first_name" />
+         Last: <input type = "text" name = "last_name" />
+         <input type = "submit" />
+      </form>
+   </body>
+</html>
