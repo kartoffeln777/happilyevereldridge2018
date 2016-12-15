1603d5d0c9a390796fbdeb1cc302e4785b9a2782
Michael Jones <michael@autorefactorer.com>
2016-12-14 22:03:49 -0600

    cleaned up main page and nearly finished the rsvp service method

--- src/index.php
+++ src/index.php
@@ -6,23 +6,17 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="My DIY wedding site">
     <meta name="author" content="michael@autorefactorer.com">
-    <link rel="icon" href="../../favicon.ico">
+    <link rel="icon" href="favicon.ico">
 
     <title>Alison and Michael's Wedding</title>
 
-    <!-- Bootstrap core CSS -->
     <link href="./bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
     <link href="cover.css" rel="stylesheet">
   </head>
 
   <body>
-
     <div class="site-wrapper">
-
       <div class="site-wrapper-inner">
-
         <div class="cover-container">
 <!--
           <div class="masthead clearfix">
@@ -45,29 +39,17 @@
             </p>
           </div>
 -->
-
           <div class="mastfoot">
             <div class="inner">
               <p>Made with love by <a href="https://autorefactorer.com">Michael</a>. Source can be found <a href="https://github.com/PisoMojado/alisonmichaelwedding">here</a>.</p>
             </div>
           </div>
-
         </div>
-
       </div>
-
     </div>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous">
-    </script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous">
-    </script>
-    <script src="./bootstrap.min.js">
-    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+    <script src="./bootstrap.min.js"></script>
   </body>
 </html>
--- src/send-rsvp/index.php
+++ src/send-rsvp/index.php
@@ -2,20 +2,23 @@
 if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0) {
     $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
     if(strcasecmp($contentType, 'application/json') != 0) {
-         die('error'.$contentType);
+         die('error: '.$contentType);
     }
     
-    print "got to this point";
-
     $content = trim(file_get_contents("php://input"));           
-    $rsvp = json_decode($content, true);
-    if(!is_array($rsvp)) {
+    $obj = json_decode($content, true);
+    if(!is_array($obj)) {
         die('error');
     }
 
-    $first_name = strtolower($rsvp['first_name']);
-    $last_name = strtolower($rsvp['last_name']);
+    print_r($obj);
+    exit();
 
+    foreach($obj->response->docs as $rsvp) {
+        $first_name = strtolower($rsvp['first_name']);
+	$last_name = strtolower($rsvp['last_name']);
+	break;
+    }
     echo "hello $first_name $last_name";
     exit();
 }
@@ -26,30 +29,43 @@ if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0) {
    <title>Test</title>
 </head>
    <body>
-      <form action = "<?php $_PHP_SELF ?>" name = "myForm" method = "POST">
-         First: <input type = "text" name = "first_name" />
-         Last: <input type = "text" name = "last_name" />
-         <input type = "submit" />
+      <form id="myForm" action="" name="myForm">
+         First: <input name="first_name" type="text"/>
+         Last: <input name="last_name" type="text"/>
+         <input id="submit" type="submit" />
       </form>
    </body>
 </html>
-   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-   <script type="text/javascript" src="https://www.json.org/json2.js"></script>
-   <script type="text/javascript">
-     $(function() {
-       var frm = $("myForm");
-       frm.submit = function(e) {
-           var dat = JSON.stringify(frm.serializeArray());
-
-           alert("I am about to POST this:\n\n" + dat);
-
-           $.post(
-             frm.attr("action"),
-             dat,
-             function(data) {
-               alert("Response: " + data);
-             }
-           );
-       }
-     });
-   </script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+<script type="text/javascript">
+function convertFormToJSON(form) {
+    var data = form.serializeArray();
+    var json = {};
+    jQuery.each(data, function() {
+        json[this.name] = this.value || '';
+    });
+    return JSON.stringify(json);
+}
+$(function() {
+    $("#myForm").submit(function() {
+        //var formData = convertFormToJSON($("#myForm"));
+        var testObj = {first_name:"michael",last_name:"jones"};
+	var formData = JSON.stringify(testObj);    
+        console.log(formData);
+	$.ajax({
+	    url: 'https://alisonmichaelwedding.com/send-rsvp/',
+            type : "POST",
+            contentType : 'application/json',
+	    data : formData,
+	    processData : false,
+	    success : function(result) {
+                console.log(result);
+            },
+            error: function(xhr, resp, text) {
+               console.log(xhr, resp, text);
+            }
+        });
+        return false;
+    });
+});
+</script>
