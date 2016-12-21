a0e55fe89b812bbadedc28bb16918424f987ab85
Michael Jones <michael@autorefactorer.com>
2016-12-20 21:29:17 -0600

    Improved lib by using symlinks, and updated Makefile to support this; added some test code for a rsvp modal

--- src/index.php
+++ src/index.php
@@ -11,8 +11,8 @@
 
     <title>Alison & Michael's Wedding</title>
 
-    <link href="./lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="./lib/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="./lib/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./lib/css/bootstrap-theme.min.css" rel="stylesheet">
     <link href="./site.css" rel="stylesheet">
   </head>
 
@@ -68,12 +68,6 @@
         In March of 2013, Alison noticed Michael across the room at a local board game gathering. He wore HotPink™ shorts, long black crew socks and sandals. At some point, Michael invited her to a round of King of Tokyo, to which Alison obliged; she trounced him. This did not deter Michael, who later asked Alison to be his teammate in a game of Ladies &amp; Gentlemen -  a game where the man treats his lady to nice things, and the lady shows the nice things off to her friends. Alison agreed, and was the Gentleman to his Lady. Together, they were the ultimate power couple and issued a resounding defeat to the rest of the table. A special relationship was born.
       </div>
 
-<!--
-      <div id="WhenWeBegan" class="inner">
-        Michael later invited Alison and some other people to an afternoon at his apartment. While leaving, Alison and Michael 
-      </div>
--->
-
       <div>
         <h3>Our Proposal</h3>
         While holidaying in Italy, Alison &amp; Michael travelled to Venice. After a day at the Basilica, marvelling at the beautiful treasure Venetian pirates had plundered from Byzantium, Michael insisted on taking a side-trip to see a modern art gallery - The <a href="https://www.google.com/maps/@45.4307284,12.335311,17.88z" target="_blank">Punta della Dogana</a>. Unbeknownst to Alison, Michael intended to propose there, as it offered a panoramic view over the Grand Canal and St. Mark's Square. To sustain the ruse, Michael had purchased tickets for entrance to the gallery before their arrival, but unfortunately for him, things had taken a dark, sinister turn since he last visited the art world. The gallery was filled with sculpture and painting that made the innocents weep, the pious unsure, and the the faint of heart fumble. With their hearts and minds forever changed, Michael brought Alison out of the gallery to the waterfront. His nerves high (was it the proposal or the art which afflicted him with nerves? he cannot say), he bent his knee and fumbled in his jacket pocket for a small box. When he looked up at Alison, he could tell that his actions so far had thankfully cleared her mind of the images from earlier. Michael recited a poem he found in a collection of love poetry he had from college - <a href="https://en.wikipedia.org/wiki/Love's_Philosophy" target="_blank">Love's Philosophy by Percy Bysshe Shelley</a>. When he finished, Michael asked Alison to marry him, to which she agreed.
@@ -86,7 +80,7 @@
 	<h3>Our Wedding</h3>
 	Alison &amp; Michael will be getting married in Houston, TX, where they have made their home. There will be a ceremony in the late afternoon with cocktails to follow. Dinner will be served in the evening. They would be honored if you attended the occasion. There will not be a place for gifts, so please ship any gifts you may have ahead of time.
         <br>
-	<div>
+	<div class="EX-details">
           <h4>The Gist</h4>
           <b>When:</b> February 19<sup>th</sup>, 2017 &emsp; <a href="" target="_blank"><span class="glyphicon glyphicon-calendar"></span> Add Event</a><br>
 	  <b>Where:</b> <a href="https://www.facebook.com/The-Wynden-391434937603142/" target="_blank">The Wynden</a>&emsp;
@@ -94,11 +88,9 @@
           <b>How:</b> With stylish dress (this is up to your interpretation)<br>
 	  <h4>The Deets</h4>
           <b>Who:</b><br>
-          <div>
-          <div>
         </div>
       </div>
-      
+
       <div id="Details" class="jumbotron section-header">
         <img class="header-image" src="./img/boat-bright.jpg">
         <h2>Details</h2>
@@ -123,22 +115,75 @@
         <img class="header-image" src="./img/boat-dark.jpg">
         <h2>RSVP</h2>
       </div>
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#rsvpModal">
+        Launch demo modal
+      </button>
 
+      <!-- Modal -->
+
+      <div class="modal fade" id="rsvpModal" tabindex="-1" role="dialog" aria-labelledby="rsvpModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="rsvpModalLabel">Modal title</h4>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+<!--
+      <div class="modal multi-step" id="rsvpModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title step-1" data-step="1">Step 1</h4>
+              <h4 class="modal-title step-2" data-step="2">Step 2</h4>
+            </div>
+            <div class="modal-body step step-1">
+              This is step 1.
+            </div>
+            <div class="modal-body step step-2">
+              This is step 2.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary step step-1" data-step="1" onclick="sendEvent()">Continue</button>
+            </div>
+          </div>
+        </div>
+      </div>
+-->
       <div class="mastfoot">
         <div class="inner">
           <p>Made with love by <a href="https://autorefactorer.com">Michael</a>. Source can be found <a href="https://github.com/PisoMojado/alisonmichaelwedding">here</a>.</p>
         </div>
       </div>
     </div> <!-- /container -->
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="./lib/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="./lib/js/bootstrap.min.js"></script>
+<!--
+    <script src="./lib/js/multi-step-modal.js></script>
+-->
     <script>
       //A script for animating the anchor links
       $('a[href*=\\#]').on('click', function(event) { 
 	event.preventDefault();
         $('html,body').animate({ scrollTop:$(this.hash).offset().top }, 400);
       });
+    </script>
+    <script>
+      sendEvent = function() {
+        $('#rsvp-modal').trigger('next.m.2');
+      }
     </script>
   </body>
 </html>
